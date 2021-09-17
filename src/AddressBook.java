79610edf0775481bb7e6a0b745af0ddd6cf7a05f
yashk <yashkapoor1102@gmail.com>
2021-09-17 05:02:57 -0400

    Added a main method that prints 'Address Book'.

---
+++ src/AddressBook.java
@@ -0,0 +1,46 @@
+/**
+ * @author  Yash Kapoor
+ * @version September 10th, 2021
+ */
+
+// Student ID: 101163338
+
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo>buddyInfo;
+
+    public AddressBook() {
+
+        this.buddyInfo = new ArrayList<BuddyInfo>();
+
+    }
+
+    public void addBuddy(BuddyInfo bi) {
+        for(int i = 0; i < buddyInfo.size(); i++)   {
+            if(bi.equals(buddyInfo.get(i))) {
+                return;
+            }
+        }
+
+        buddyInfo.add(bi);
+    }
+
+    public void removeBuddy(BuddyInfo bi) {
+        for(int i = 0; i < buddyInfo.size(); i++)  {
+            if(bi.equals(buddyInfo.get(i))) {
+                buddyInfo.remove(bi);
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+
+}
+
+
+
